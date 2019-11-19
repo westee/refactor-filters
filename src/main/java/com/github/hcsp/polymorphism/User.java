02eaadcaac87fe3ae8a733219549a099348dcac6
dflk <904885508@qq.com>
2019-11-19 22:44:01 +0800

    使用java8::语法,使用lambda表达式，使用静态方法，实例方法

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -5,14 +5,7 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class User {
-    /**
-     * 用户ID，数据库主键，全局唯一
-     */
     private final Integer id;
-
-    /**
-     * 用户名
-     */
     private final String name;
 
     public User(Integer id, String name) {
@@ -28,28 +21,30 @@ public class User {
         return name;
     }
 
+    // 实例方法 实际有一个this参数
+    public boolean isLi(){
+        return this.name.startsWith("王");
+    }
+
+    // 静态方法
+    private static boolean userWithEvenId(User user){
+        return user.getId() % 2 == 0;
+    }
+
     // 过滤ID为偶数的用户
     public static List<User> filterUsersWithEvenId(List<User> users) {
-        return filter(users, new filterWithIdIsEven());
+        // Java 8 中我们可以通过 `::` 关键字来访问类的构造方法，对象方法，静态方法。
+        return filter(users, User::userWithEvenId);
     }
 
     // 过滤姓张的用户
     public static List<User> filterZhangUsers(List<User> users) {
-        return filter(users, new startWithZhang());
+        // lambda表达式
+        return filter(users, (Predicate<User>) user -> user.getName().startsWith("张"));
     }
 
-    static class startWithZhang implements Predicate<User> {
-        @Override
-        public boolean test(User user) {
-            return false;
-        }
-    }
-
-    static class filterWithIdIsEven implements Predicate<User> {
-        @Override
-        public boolean test(User user) {
-            return user.id % 2 == 0;
-        }
+    public static List<User> filterLi(List<User> users){
+        return filter(users, User::isLi);
     }
 
     // 过滤姓王的用户
@@ -65,10 +60,8 @@ public class User {
         }
     }
 
-    // 你可以发现，在上面三个函数中包含大量的重复代码。
-    // 请尝试通过Predicate接口将上述代码抽取成一个公用的过滤器函数
     // 并简化上面三个函数
-    public static List<User> filter(List<User> users, Predicate predicate) {
+    public static List<User> filter(List<User> users, Predicate<User> predicate) {
         List<User> results = new ArrayList<>();
         for (User user : users) {
             if (predicate.test(user)) {
